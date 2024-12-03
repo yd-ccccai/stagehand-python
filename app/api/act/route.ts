bdff1e85836e6a3fba51f43c962e6197bb7abc89
Anirudh Kamath <github@kamath.io>
2024-12-02 22:33:25 -0800

    some routes working

---
+++ app/api/act/route.ts
@@ -0,0 +1,65 @@
+// app/api/stream/route.ts
+import { AvailableModel, LogLine, Stagehand } from "@browserbasehq/stagehand";
+import { NextResponse } from "next/server";
+import { ActionLogger } from "@/lib/actionLogger";
+const env: "BROWSERBASE" | "LOCAL" = "BROWSERBASE";
+const modelName: AvailableModel = "gpt-4o";
+const domSettleTimeoutMs = 10000;
+const enableCaching = process.env.EVAL_ENABLE_CACHING?.toLowerCase() === "true";
+
+const defaultStagehandOptions = {
+  env,
+  headless: false,
+  verbose: 2 as const,
+  debugDom: true,
+  enableCaching,
+};
+
+const initStagehand = async (
+  logger: ActionLogger,
+  controller: ReadableStreamController<Uint8Array>,
+  encoder: TextEncoder
+): Promise<Stagehand> => {
+  const stagehand = new Stagehand({
+    ...defaultStagehandOptions,
+    logger: (logLine: LogLine) => {
+      logger.log(logLine);
+    },
+  });
+  logger.init(stagehand, controller, encoder);
+  await stagehand.init({ modelName, domSettleTimeoutMs });
+  return stagehand;
+};
+
+export async function GET() {
+  // Create a readable stream
+  const stream = new ReadableStream({
+    async start(controller) {
+      const logger = new ActionLogger();
+      const encoder = new TextEncoder();
+      await initStagehand(logger, controller, encoder);
+
+      //   await stagehand.page.goto("https://github.com/browserbase/stagehand");
+      //   await stagehand.act({ action: "click on the contributors" });
+      //   const contributor = await stagehand.extract({
+      //     instruction: "extract the top contributor",
+      //     schema: z.object({
+      //       username: z.string(),
+      //       url: z.string(),
+      //     }),
+      //   });
+      //   console.log("our favorite contributor is", contributor);
+
+      controller.close();
+    },
+  });
+
+  // Return the stream with appropriate headers
+  return new NextResponse(stream, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+    },
+  });
+}
---
+++ lib/actionLogger.ts
@@ -0,0 +1,78 @@
+import { LogLine } from "@browserbasehq/stagehand";
+import { Stagehand } from "@browserbasehq/stagehand";
+
+function logLineToString(logLine: LogLine): string {
+  const timestamp = logLine.timestamp || new Date().toISOString();
+  if (logLine.auxiliary?.error) {
+    return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message}\n ${logLine.auxiliary.error.value}\n ${logLine.auxiliary.trace.value}`;
+  }
+  return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message} ${
+    logLine.auxiliary ? JSON.stringify(logLine.auxiliary) : ""
+  }`;
+}
+
+type LogLineAction = LogLine & {
+  parsedAuxiliary?: string | object;
+};
+
+function parseLogLine(logLine: LogLine): LogLineAction {
+  return {
+    ...logLine,
+    auxiliary: undefined,
+    parsedAuxiliary: logLine.auxiliary
+      ? Object.fromEntries(
+          Object.entries(logLine.auxiliary).map(([key, entry]) => [
+            key,
+            entry.type === "object" ? JSON.parse(entry.value) : entry.value,
+          ])
+        )
+      : undefined,
+  } as LogLineAction;
+}
+
+export class ActionLogger {
+  initialized = false;
+  logs: LogLineAction[] = [];
+  stagehand?: Stagehand;
+  controller?: ReadableStreamController<Uint8Array>;
+  encoder?: TextEncoder;
+
+  constructor() {}
+
+  init(
+    stagehand: Stagehand,
+    controller: ReadableStreamController<Uint8Array>,
+    encoder: TextEncoder
+  ) {
+    this.stagehand = stagehand;
+    this.controller = controller;
+    this.encoder = encoder;
+    this.initialized = true;
+    console.log("ActionLogger initialized");
+  }
+
+  log(logLine: LogLine) {
+    if (!this.initialized) {
+      throw new Error("ActionLogger not initialized");
+    }
+    console.log(logLineToString(logLine));
+    // this.controller.enqueue(
+    //   this.encoder.encode(logLineToString(logLine) + "\n")
+    // );
+    // this.logs.push(parseLogLine(logLine));
+  }
+
+  error(logLine: LogLine) {
+    console.error(logLineToString(logLine));
+    this.logs.push(parseLogLine(logLine));
+  }
+
+  warn(logLine: LogLine) {
+    console.warn(logLineToString(logLine));
+    this.logs.push(parseLogLine(logLine));
+  }
+
+  getLogs() {
+    return this.logs;
+  }
+}
